3f7a868f437b5b92030bd52d7d0fce1e58e38dfe
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-02-15 13:12:16 +0530

    updated components

--- server/server.js
+++ server/server.js
@@ -18,13 +18,30 @@ import { studentCoursesRoutes } from "./routes/student-routes/student-courses-ro
 import { studentCourseProgressRoutes } from "./routes/student-routes/course-progress-routes.js";
 
 //middleware configuration
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   },
+// )
+// );
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        process.env.CLIENT_URL,
+        "http://localhost:3000", // Add other origins as needed
+      ];
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  },
-)
+  })
 );
 
 app.use(express.json());
